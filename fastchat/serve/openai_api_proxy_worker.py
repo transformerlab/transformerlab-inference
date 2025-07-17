0b96fadf8846d7f4b10af97def16d249d69f03f9
Mina Parham <minaparham@Tonys-MacBook-Pro.local>
2025-07-16 22:08:33 -0400

    Use logger for debugging

--- fastchat/serve/openai_api_proxy_worker.py
+++ fastchat/serve/openai_api_proxy_worker.py
@@ -69,7 +69,7 @@ class OpenAIWorker(BaseModelWorker):
 
 # Required param: "type" must be either "completion" or "chat-completion"
     async def generate_stream(self, params):
-        print(f"Generating stream with params: {params}",  file=sys.stderr)
+        logger.info(f"Generating stream with params: {params}")
         self.call_ct += 1
         
         type_ = params.get("type", "completion")
