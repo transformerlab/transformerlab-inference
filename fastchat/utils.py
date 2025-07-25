032b24fe0a80b6e820588c236cf7bdb97b3049bc
mina-parham <mina.parham766@gmail.com>
2025-07-25 10:04:18 -0400

    Remove comment

--- fastchat/utils.py
+++ fastchat/utils.py
@@ -28,8 +28,6 @@ visited_loggers = set()
 
 
 def build_logger(logger_name, logger_filename):
-    # global handler
-
     formatter = logging.Formatter(
         fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
